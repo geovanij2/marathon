063d59e0b7aab16759c5403644b06c23e65c757b
Geovani da Silva Júnior <geovanij3@gmail.com>
2018-04-15 23:06:27 -0300

    Modify code to read 3 entries in 1 scanf

--- lista1/ex5.c
+++ lista1/ex5.c
@@ -3,9 +3,7 @@
 int main(int argc, char const *argv[])
 {
 	int c, p, f;
-	scanf("%d", &c);
-	scanf("%d", &p);
-	scanf("%d", &f);
+	scanf("%d%d%d", &c, &p, &f);
 	if (c * f > p)
 		printf("N\n");
 	else
